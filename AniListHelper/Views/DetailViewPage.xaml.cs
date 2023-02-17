2e37d40e4fb03b9ed0c9b5bfe16b3ee37b30bfb9
Uk Gorsi <chaudhry_uk_gorsi@hotmail.com>
2023-02-17 23:39:09 +0500

    minor changes

--- AniListHelper/Views/DetailViewPage.xaml.cs
+++ AniListHelper/Views/DetailViewPage.xaml.cs
@@ -5,7 +5,10 @@ namespace AniListHelper.Views;
 public partial class DetailViewPage : ContentPage {
     public DetailViewPage(MediaEntryModel selectedItem) {
         InitializeComponent();
+        //LblImage.Source = selectedItem.Image;
+        LblImage.Source = "https://picsum.photos/seed/picsum/200/300";
         LblName.Text = selectedItem.Name;
+        LblName.LineBreakMode = LineBreakMode.MiddleTruncation;
         LblStatus.Text = selectedItem.Status;
         LblOtherNames.Text = selectedItem.OtherNames;
         //LblDescription.Text = selectedItem.Description;
