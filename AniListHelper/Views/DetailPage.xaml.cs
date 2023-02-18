6584881c923cae030949059f8e88ef68a4bfd034
Uk Gorsi <chaudhry_uk_gorsi@hotmail.com>
2023-02-18 15:46:36 +0500

    Added cover photo on detail page

--- AniListHelper/Views/DetailPage.xaml.cs
+++ AniListHelper/Views/DetailPage.xaml.cs
@@ -5,7 +5,8 @@ namespace AniListHelper.Views;
 public partial class DetailPage : ContentPage {
     public DetailPage(MediaEntryModel selectedItem) {
         InitializeComponent();
-        LblImage.Source = "https://picsum.photos/seed/picsum/200/300";
+        //LblImage.Source = "https://picsum.photos/seed/picsum/200/300";
+        LblImage.Source = "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21230-rfoUZud1Jn0L.png";
         LblName.Text = selectedItem.Name;
         LblStatus.Text = selectedItem.Status;
         LblOtherNames.Text = selectedItem.OtherNames;
