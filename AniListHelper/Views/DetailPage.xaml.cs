b3deb040d297beaaf9b1f02ac17c086005af139b
Uk Gorsi <chaudhry_uk_gorsi@hotmail.com>
2023-02-18 16:20:18 +0500

    Fixed the text wrapping issue

--- AniListHelper/Views/DetailPage.xaml.cs
+++ AniListHelper/Views/DetailPage.xaml.cs
@@ -9,6 +9,6 @@ public partial class DetailPage : ContentPage {
         LblImage.Source = "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21230-rfoUZud1Jn0L.png";
         LblName.Text = selectedItem.Name;
         LblStatus.Text = selectedItem.Status;
-        LblOtherNames.Text = selectedItem.OtherNames;
+        //LblOtherNames.Text = selectedItem.OtherNames;
     }
 }
