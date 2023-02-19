9e827712cfa3f4f97fd4227d255cc70feeccbca6
Uk Gorsi <chaudhry_uk_gorsi@hotmail.com>
2023-02-19 17:44:03 +0500

    minor updates

--- AniListHelper/Views/DetailPage.xaml.cs
+++ AniListHelper/Views/DetailPage.xaml.cs
@@ -5,13 +5,11 @@ namespace AniListHelper.Views;
 public partial class DetailPage : ContentPage {
     public DetailPage(MediaEntryModel selectedItem) {
         InitializeComponent();
-        //LblImage.Source = "https://picsum.photos/seed/picsum/200/300";
         LblImage.Source = "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21230-rfoUZud1Jn0L.png";
         LblName.Text = selectedItem.Name;
+        LblHeading.Text = selectedItem.Name;
         LblStatus.Text = selectedItem.Status;
-        //LblStart.Text = selectedItem.startDate;
-        //LblEnd.Text = selectedItem.endDate;
-        //LblOtherNames.Text = selectedItem.OtherNames;
+        LblOtherNames.Text = selectedItem.OtherNames;
     }
     private void Back_Tapped(object sender, EventArgs e) {
         Navigation.PopAsync();
